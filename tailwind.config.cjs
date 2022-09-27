5e253c2181c653dce9b472502a9d0521ba2d84f5
Tashif Ferdous <tashferd07@gmail.com>
2022-09-27 14:18:29 -0400

    update tailwind themes and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        mono: ["Fira Code", ...defaultTheme.fontFamily.mono],
       },
       colors: {
         primary: "var(--color-primary)",
