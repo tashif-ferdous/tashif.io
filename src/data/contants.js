ab9c968abc1e13ba6d9950d41af738686c80819d
Tashif Ferdous <tashferd07@gmail.com>
2022-10-03 23:51:06 -0400

    move links to constants, add footer

--- src/data/contants.js
+++ src/data/contants.js
@@ -1,2 +1,4 @@
 export const RESUME_URL = "/assets/pdf/Tashif-Ferdous-Resume-2022-Sep-Public.pdf";
-export const MAILTO_URL = "mailto:tashferd07@gmail.com"
+export const MAILTO_URL = "mailto:tashferd07@gmail.com";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/tferd/";
+export const GITHUB_URL = "https://github.com/tashif-ferdous";
